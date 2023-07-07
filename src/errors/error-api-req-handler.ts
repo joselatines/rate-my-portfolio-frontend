03070c4da2aa456770cd1aa4c6cfb6925ef5b3bc
joselatines <joselatines33@gmail.com>
2023-07-07 10:37:51 -0400

    fix(error handler): add types to arror handler

--- src/errors/error-api-req-handler.ts
+++ src/errors/error-api-req-handler.ts
@@ -1,4 +1,22 @@
-export function errorApiReqHandler(error: any) {
-	// console.error("⚠️: ", error);
-	return { success: false, message: error.message };
+import { AxiosError } from "axios";
+
+interface CustomError {
+	success: false;
+	message: string;
+}
+
+export function errorApiReqHandler(
+	error: AxiosError | Error | any
+): CustomError {
+	console.error("⚠️: ", error);
+	let message = error.message;
+
+	if (error instanceof AxiosError && error.response) {
+		message = error.response.data.message || message;
+	}
+
+	return {
+		success: false,
+		message: message,
+	};
 }
